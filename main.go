bde3374c4b73b27e122e2f932bce6983b25ecd43
muzry <lidan@caicloud.io>
2018-09-20 22:20:54 +0800

    update for front end.

--- main.go
+++ main.go
@@ -1,52 +1,56 @@
 package main
 
 import (
-	"math/rand"
-	"time"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"math/rand"
 	"net/http"
+	"time"
+	"github.com/gin-contrib/cors"
 	)
 
-type NumberInfo struct{
-	UserName 	string
-	Number		int
-	IP			string
+type NumberInfo struct {
+	UserName string `json:"username"`
+	Number   int    `json:"number"`
+	IP       string `json:"ip"`
 }
 
 var InfoList []NumberInfo
 
-func getNumberByRandom(ctx *gin.Context){
+func getNumberByRandom(ctx *gin.Context) {
 	numberinfo := NumberInfo{}
 	numberinfo.IP = ctx.ClientIP()
 	username := ctx.DefaultQuery("username", "Anonymous")
+	if username == "Anonymous"{
+		ctx.JSON(http.StatusBadRequest, nil)
+		return
+	}
+
 	numberinfo.UserName = username
 	numberinfo.Number = getRandomNumber()
 	removeRepeat(numberinfo)
-	ctx.String(http.StatusOK, fmt.Sprintf("%sRoll的点数为%d。", username, numberinfo.Number))
+	ctx.JSON(http.StatusOK, gin.H{
+		"username": username,
+		"number":   numberinfo.Number,
+	})
 }
 
-func removeRepeat(numberInfo NumberInfo){
-	for _, element := range InfoList{
+func removeRepeat(numberInfo NumberInfo) {
+	for _, element := range InfoList {
 		if element.IP == numberInfo.IP {
 			return
 		}
 	}
 	InfoList = append(InfoList, numberInfo)
-
 }
 
-func getStatistic(ctx * gin.Context){
-	templateString := "%d, IP地址为%s的用户%sRoll的点数为%d。\n"
-	resultString:= ""
-	if len(InfoList) > 0 {
-		for index, element := range InfoList {
-			resultString += fmt.Sprintf(templateString,index + 1, element.IP, element.UserName, element.Number)
-		}
-		ctx.String(http.StatusOK, resultString)
-	} else{
-		ctx.String(http.StatusOK, "没有记录。")
-	}
+func getStatistic(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, InfoList)
+}
+
+func clearStatistic(ctx *gin.Context) {
+	InfoList = make([]NumberInfo, 0)
+	ctx.JSON(http.StatusNoContent, nil)
 }
 
 func getRandomNumber() int {
@@ -54,11 +58,17 @@ func getRandomNumber() int {
 	return rand.Intn(100) + 1
 }
 
-
-func main(){
+func main() {
 	fmt.Println(getRandomNumber())
 	router := gin.Default()
 	router.GET("/api/v1alpha1/number", getNumberByRandom)
 	router.GET("/api/v1alpha1/result", getStatistic)
+	router.DELETE("/api/v1alpha1/result", clearStatistic)
+
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	config.AllowHeaders = []string{"Content-Type"}
+	router.Use(cors.New(config))
+
 	router.Run(":8080")
 }
